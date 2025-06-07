3b03bce0475dbbdf2ac6c5f3afb413ed4ee4cc3d
njcamun <ncamungondo@gmail.com>
2025-06-07 09:40:59 +0100

    Delete StartPythonServer.bat

--- StartPythonServer.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem ----------------------------------------------------
-rem  Inicia servidor HTTP simples com Python na porta 8000
-rem ----------------------------------------------------
-echo Iniciando servidor Python na porta 8000...
-python -m http.server 8000
-pause
